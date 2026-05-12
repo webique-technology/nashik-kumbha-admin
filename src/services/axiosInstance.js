6228c47651cbf3ab0b94c473acff05c39400c311
webique-technology <webiquetech@gmail.com>
2026-05-12 15:07:35 +0530

    make aniket name branch and push all the code in aniket branch

---
+++ src/services/axiosInstance.js
@@ -0,0 +1,39 @@
+// import axios from 'axios';
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// console.log("Base URL", api.baseURL);
+
+// Req Interceptor: Securely attach tokens
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        console.log("token", token);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+// Res Interceptor: Global error handling
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            // Logic to logout user or refresh token
+            console.error('Unauthorized, redirecting to login...');
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default api;
